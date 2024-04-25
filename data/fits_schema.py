9368858491c0936a3c5a8e58a63c57a717a4f180
mclrc <moritzamando@proton.me>
2024-04-25 22:13:34 +0200

    Add plotting script

--- data/fits_schema.py
+++ data/fits_schema.py
@@ -5,11 +5,18 @@ import sys
 def print_fits_schema(file_path):
     with fits.open(file_path) as hdul:
         hdul.info()
-        print(hdul[1])
-        data = hdul[1].data
-        print(data.columns)
-        for column in data.columns:
-            print(column.name, column.format)
+        for hdu in hdul:
+            print(hdu.header)
+
+            if hdu.data is None:
+                continue
+
+            data = hdu.data
+
+            print(data.columns)
+
+            for column in data.columns:
+                print(column.name, column.format, column.unit)
 
 if __name__ == "__main__":
     print_fits_schema(sys.argv[1])
