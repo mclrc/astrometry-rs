3dcce895ea4592de0a379e94eb38258fad1c37e4
Moritz <moritzamando@proton.me>
2024-04-26 19:56:12 +0200

    Update catalog schema

--- index_builder/src/main.rs
+++ index_builder/src/main.rs
@@ -1,19 +1,32 @@
 use std::error::Error;
 
 use clap::Parser;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 use common::fits::FitsTable;
+use serde_json::Value;
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 #[allow(dead_code)]
 struct CatalogObject {
     #[serde(rename = "USNOB_ID")]
     usnob_id: i32,
     #[serde(rename = "RA")]
     ra: f64,
+    #[serde(rename = "SIGMA_RA")]
+    sigma_ra: f32,
+    #[serde(rename = "SIGMA_RA_FIT")]
+    sigma_ra_fit: f32,
+    #[serde(rename = "PM_RA")]
+    pm_ra: f32,
     #[serde(rename = "DEC")]
     dec: f64,
+    #[serde(rename = "SIGMA_DEC")]
+    sigma_dec: f32,
+    #[serde(rename = "SIGMA_DEC_FIT")]
+    sigma_dec_fit: f32,
+    #[serde(rename = "PM_DEC")]
+    pm_dec: f32,
     #[serde(rename = "MAGNITUDE_0")]
     mag0: f32,
     #[serde(rename = "MAGNITUDE_1")]
@@ -24,6 +37,12 @@ struct CatalogObject {
     mag3: f32,
     #[serde(rename = "MAGNITUDE_4")]
     mag4: f32,
+    #[serde(rename = "EPOCH")]
+    epoch: f32,
+    #[serde(rename = "NUM_DETECTIONS")]
+    num_detections: i32,
+    #[serde(rename = "FLAGS")]
+    flags: u8,
 }
 
 #[derive(Parser, Debug)]
@@ -39,8 +58,21 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     println!("{}", table.len());
 
-    for (name, _) in table.columns().iter() {
-        println!("{}", name);
+    for (name, column) in table.columns().iter() {
+        println!("{}: {}", name, column.format());
+    }
+
+    for row in table.iter::<CatalogObject>() {
+        let row = row?;
+        let zone = (row.ra * 10f64).floor() as i32;
+
+        let vizier_id = format!("{:04}-{:07}", zone, row.usnob_id);
+
+        println!(
+            "{}: {}",
+            vizier_id,
+            serde_json::to_string_pretty(&row).unwrap()
+        );
     }
 
     Ok(())
